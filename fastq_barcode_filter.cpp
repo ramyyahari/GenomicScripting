d1dfae8dc2e8985aa32d0e5b34099478ad70416e
Ramyya Hari <ramyya@gmail.com>
2017-05-30 13:41:12 -0700

    Update fastq_barcode_filter.cpp

--- fastq_barcode_filter.cpp
+++ fastq_barcode_filter.cpp
@@ -1,3 +1,12 @@
+/*
+Script to identify barcodes(in header) from fastq files.
+Modify lines 39, 40, 41 to add or remove barcodes, also change respective variables in line 43.
+Modify input filename on line 26
+To compile:
+    g++ -std=c++11 fastq_barcode_splitter.cpp -o fastq_barcode_splitter
+To run:
+    fastq_barcode_splitter
+*/
 #include<iostream>
 #include<string>
 #include<algorithm>
@@ -31,11 +40,11 @@ int main(){
         read.quality_score=tmp;
         if(tmp.empty())
             continue;
-        /*
+        
         size_t barcode_1=read.read.find("TTCGTGATTC");
         size_t barcode_2=read.read.find("TTGGAGTGTC");
         size_t barcode_3=read.read.find("TTGGCATCTC");
-        */
+        
             if(barcode_1==0||barcode_2==0||barcode_3==0)
             cout<<read.header<<"\n"<<read.read<<"\n+\n"<<read.quality_score<<endl;
     }
